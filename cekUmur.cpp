170ed461a8e14ac68c8442e0cfb4d348a23490ce
Ragil}{P <44290386+InYourG00D1@users.noreply.github.com>
2020-04-20 17:56:44 +0700

    Add files via upload

---
+++ cekUmur.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/* Code By Ragil 
+Team SoulTeamCyber
+Semoga Berguna :v
+*/
+
+int main(){
+	string nama;
+	int tahun;
+	cout << "+====================InYourG00D+" <<endl;
+ cout << "+                              +" <<endl;
+ cout << "+        Mengecek Umur         +" <<endl;
+ cout << "+                              +" <<endl;
+ cout << "+=========================Ragil+" <<endl;
+	cout << "Nama Anda : ";
+	cin >> nama;
+	cout << "Tahun Anda Lahir : ";
+	cin >> tahun;
+	cout << "_____________________________________________" <<endl;
+	cout << " Hai " << nama <<endl;
+	cout << " Umur Anda " << 2020 - tahun << " Tahun " << " Nampaknya Anda Sudah Semakin Tua" <<endl;
+}
